e9c21e35dcf5396fa43c1b9c52320a8f4f697f42
L. C. Sundar Raj <37692946+Lcsundarraj@users.noreply.github.com>
2024-03-20 12:40:13 +0530

    changes of mar15

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request
 import datetime
 
+import get_form_data
+import id_generator
+
 app = Flask(__name__)
 app.secret_key = 'jdjhd67d873hdihd38id38h'
 
@@ -17,7 +20,19 @@ def login():
 def signup():
     return render_template('signup.html')
 
+@app.route('/register', methods=['POST'])
+def register_details():
+    user_data = get_form_data.signup_form_data()
+    print("form data = ", user_data)
+    user_data['user_id'] = 
+    user_data['regd_date'] = datetime.datetime.today()
+    print("form data = ", user_data)
+    return redirect(url_for('success'))
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
