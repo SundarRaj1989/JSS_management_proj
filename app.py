9b6878ba59368f123f6be775e4e38275626c043b
L. C. Sundar Raj <37692946+Lcsundarraj@users.noreply.github.com>
2024-04-03 13:16:37 +0530

    registration code

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 import datetime
 
 import get_form_data
 import id_generator
+import authentication_db
 
 app = Flask(__name__)
 app.secret_key = 'jdjhd67d873hdihd38id38h'
@@ -23,11 +24,21 @@ def signup():
 @app.route('/register', methods=['POST'])
 def register_details():
     user_data = get_form_data.signup_form_data()
-    print("form data = ", user_data)
-    user_data['user_id'] = 
-    user_data['regd_date'] = datetime.datetime.today()
-    print("form data = ", user_data)
-    return redirect(url_for('success'))
+    email = user_data['email_id']
+    account_existance_check = authentication_db.check_user_availability(email)
+    if account_existance_check:
+        print('User already exists')
+        flash("User already exists!! You need to Login")
+        return redirect(url_for('login'))
+    else:
+        print('New user')
+        cy = datetime.datetime.today()
+        current_year = cy.strftime("%y") 
+        user_data['regd_date'] = cy
+        user_data['user_id'] = id_generator.random_alpha_numeric_id_with_currentYear(1, 5, current_year)
+        msg = authentication_db.save_user_details(user_data)
+        flash(msg)
+        return redirect(url_for('success'))
 
 @app.route('/success')
 def success():
--- id_generator.py
+++ id_generator.py
@@ -2,6 +2,7 @@ import random
 import string
 
 
+#letters according to number + digits according to number [e.g. L-3 D-5 = GKU51230]
 def random_alpha_numeric_id(letters_count, digits_count):
     sample_str = ''.join((random.choice(string.ascii_uppercase) for i in range(letters_count)))
     sample_str += ''.join((random.choice(string.digits) for i in range(digits_count)))
@@ -9,9 +10,23 @@ def random_alpha_numeric_id(letters_count, digits_count):
     final_string = ''.join(sample_list)
     return final_string
 
+#passed fixed letter + digits according to passed number [e.g. Fixed_L-JSS, D-4 = JSS5032 ]
 def fixed_alpha_random_number_id(letters, digits_count):
     sample_str = letters
     sample_str += ''.join((random.choice(string.digits) for i in range(digits_count)))
     sample_list = list(sample_str)
     final_string = ''.join(sample_list)
     return final_string
+
+
+
+
+#Fixed letters JSS  + last 2 digit of current year + random sigle letter + digits according to passed number [e.g. Fixed_L-JSS, D-4 = JSS5032 ]
+def random_alpha_numeric_id_with_currentYear(letters_count, digits_count, current_year):
+    brand_name = 'JSS'    
+    rndm_letter = ''.join(random.choice(string.ascii_uppercase) for i in range(letters_count))
+    rndm_number = ''.join(random.choice(string.digits) for i in range(digits_count))
+    rndm_alph_num = str(rndm_letter) + str(rndm_number)  
+    rndm_id_tupple = (brand_name, current_year, rndm_alph_num)
+    random_id_str = "-".join(rndm_id_tupple)
+    return random_id_str
